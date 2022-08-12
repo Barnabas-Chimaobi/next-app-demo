99011c6b10d7967e2d265ebb76d9c95d9e526f94
Matthew Barnabas <enyinnayabarnabas@gmail.com>
2022-08-12 06:58:51 +0100

    homepage and payment page fixed

---
+++ components/nav/index.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import Head from 'next/head'
+const Navbar = () => {
+  return (
+    <Head>
+    <header className="row">
+      <nav className="navbar navbar-expand-lg justify-content-between">
+        <a className="navbar-brand" href="#">
+          <img
+            src="ilaro-logo.jpeg"
+            style={{ height: 50, width: 50, padding: 5, marginLeft: 30 }}
+            className="rounded"
+          />
+        </a>
+        <div className="">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <div className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle text-white" href="#">
+                  Applicant
+                </a>
+              </li>
+              <div className="dropdown">
+                <li className="nav-item ">
+                  <a className="nav-link dropdown-toggle text-white" href="#">
+                    Returning Student
+                  </a>
+                  <div className="dropdown-menu">
+                    <a href="" className="dropdown-item">
+                      ..
+                    </a>
+                    <a href="" className="dropdown-item">
+                      ..
+                    </a>
+                  </div>
+                </li>
+              </div>
+              <li className="nav-item">
+                <a className="nav-link dropdown-toggle text-white" href="#">
+                  Graduate
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  Complaints
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  User Guide
+                </a>
+              </li>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header></Head>
+  )
+}
+
+export {Navbar}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,67 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+// // import 'https://fonts.cdn.com/css/gilroy-bold'
 
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import Head from "next/head"
+import Script from "next/script"
+import { Navbar } from "../components/nav"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+  <Head>
+    <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ilaro Poly</title>
+    <link rel="stylesheet" href="css/index.css"/>
+    <link rel="stylesheet" href="css/ilaroStyles.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+      integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+      crossOrigin="anonymous"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+      crossOrigin="anonymous"
+    />
+     <link rel="preconnect" href=" http://fonts.cdnfonts.com/css/gilroy-bold"
+      />
+  </Head>
+    <Navbar/>
+        <Component {...pageProps} />
+         {/* FOOTER */}
+  {/* <footer className="footer py-5">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 col-12">
+          <p className="copyright-text text-center">
+            Copyright © 2019 Company Name . All rights reserved
+          </p>
+          <p className="copyright-text text-center">
+            Designed by{" "}
+            <a rel="nofollow" href="https://www.facebook.com/tooplate">
+              Tooplate
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer> */}
+   {/* <Script src="js/jquery-3.3.1.min.js"></Script>
+    <Script src="js/popper.min.js"></Script>
+    <Script src="js/bootstrap.min.js"></Script>
+    <Script src="js/custom.js"></Script> */}
+
+    </>
+  )
 }
 
 export default MyApp
