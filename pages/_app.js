640b64bccf9d29a3628bb7933bce225d1299d6bf
Christian Nnabugwu <Christiannnabugwu8@gmail.com>
2022-08-25 17:19:59 +0100

    LOGIN UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,52 +1,63 @@
-import Head from "next/head"
-import Script from "next/script"
-import { Navbar } from "../components/nav"
-import { ApolloClient, ApolloLink, InMemoryCache, HttpLink, ApolloProvider } from "@apollo/client";
+import Head from "next/head";
+import Script from "next/script";
+import { Navbar } from "../components/nav";
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  HttpLink,
+  ApolloProvider,
+} from "@apollo/client";
 import { client } from "../api";
-import Frontlayout from '../Layouts/FrontLayout/frontlayout'
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import '../styles/globals.css'
+import Frontlayout from "../Layouts/FrontLayout/frontlayout";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "../styles/globals.css";
 
 // import { RestLink } from "apollo-link-rest";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-  <Head>
-    <meta charSet="UTF-8" />
-    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Ilaro Poly</title>
-    <link rel="stylesheet" href="css/index.css"/>
-    <link rel="stylesheet" href="css/ilaroStyles.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-      integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
-      crossOrigin="anonymous"
-    />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
-      crossOrigin="anonymous"
-    />
-     <link rel="preconnect" href=" http://fonts.cdnfonts.com/css/gilroy-bold"
-      />
-    <script src="https://kit.fontawesome.com/18846ba9d4.js" crossorigin="anonymous"></script>  
-  </Head>
-     {/* <Navbar/>  */}
-    <ApolloProvider client={client}>
-     {/* <Frontlayout> */}
-       <Component {...pageProps}  />
-     {/* </Frontlayout> */}
-    </ApolloProvider>
-        {/* <main>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Ilaro Poly</title>
+        <link rel="stylesheet" href="css/index.css" />
+        <link rel="stylesheet" href="css/ilaroStyles.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+          integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href=" http://fonts.cdnfonts.com/css/gilroy-bold"
+        />
+        <script
+          src="https://kit.fontawesome.com/18846ba9d4.js"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      {/* <Navbar/>  */}
+      <ApolloProvider client={client}>
+        <Frontlayout>
+          <Component {...pageProps} />
+        </Frontlayout>
+      </ApolloProvider>
+      {/* <main>
           {children}
         </main> */}
-        
-         {/* FOOTER */}
-  {/* <footer className="footer py-5">
+
+      {/* FOOTER */}
+      {/* <footer className="footer py-5">
     <div className="container">
       <div className="row">
       give me a sec
@@ -64,13 +75,12 @@ function MyApp({ Component, pageProps }) {
       </div>
     </div>
   </footer> */}
-   {/* <Script src="js/jquery-3.3.1.min.js"></Script>
+      {/* <Script src="js/jquery-3.3.1.min.js"></Script>
     <Script src="js/popper.min.js"></Script>
     <Script src="js/bootstrap.min.js"></Script>
     <Script src="js/custom.js"></Script> */}
-
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
